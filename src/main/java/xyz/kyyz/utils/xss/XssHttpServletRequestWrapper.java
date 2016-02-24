f94092b9c4869439f6820a84d4cf900dca02d845
kuangye <kuangye@htke.com>
2016-02-24 09:27:58 +0800

    1.defend xss

---
+++ src/main/java/xyz/kyyz/utils/xss/XssHttpServletRequestWrapper.java
@@ -0,0 +1,51 @@
+package xyz.kyyz.utils.xss;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * Created by ky on 16/1/21.
+ */
+public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+    /**
+     * Constructs a request object wrapping the given request.
+     *
+     * @param request
+     * @throws IllegalArgumentException if the request is null
+     */
+    public XssHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return StringEscapeUtils.escapeHtml4(super.getHeader(name));
+    }
+
+    @Override
+    public String getQueryString() {
+        return StringEscapeUtils.escapeHtml4(super.getQueryString());
+    }
+
+    @Override
+    public String getParameter(String name) {
+        return StringEscapeUtils.escapeHtml4(super.getParameter(name));
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] values = super.getParameterValues(name);
+        if (values != null) {
+            int length = values.length;
+            String[] escapseValues = new String[length];
+            for (int i = 0; i < length; i++) {
+                escapseValues[i] = StringEscapeUtils.escapeHtml4(values[i]);
+            }
+            return escapseValues;
+        }
+        return super.getParameterValues(name);
+    }
+}
